4227ecb40ed6ea69764be7322be8659f3b5bdeec
Coderboy80 <65282069+Coderboy80@users.noreply.github.com>
2022-10-09 13:08:49 +0530

    add: information boxes under titles, colors for it

--- index.js
+++ index.js
@@ -4,12 +4,45 @@ const postBtn = document.querySelector(".post-btn");
 const header = document.querySelector("header");
 const main = document.querySelector(".main");
 
-function generateCode(codeText, codeClassesName = ["default-resource"]) {
-  let code = document.createElement("code");
-  code.innerHTML = `<div>${codeText}</div>`;
-  code.classList.add("default-resource");
-  code.classList.add(...codeClassesName);
-  main.appendChild(code);
+let resourceTitle;
+let resourceInformation;
+
+function generateCode(
+  resourceTitleText,
+  resourceTitleClassesName = ["default-resource"],
+  resourceInfo = "Default information please fill this.",
+  resourceInfoClassesName = ["default-resource-info"]
+) {
+  resourceTitle = document.createElement("div");
+  resourceInformation = document.createElement("p");
+  resourceTitle.innerHTML = `${resourceTitleText}`;
+  resourceInformation.innerHTML = `${resourceInfo}`;
+  resourceTitle.classList.add("default-resource");
+  resourceTitle.classList.add(...resourceTitleClassesName);
+  resourceInformation.classList.add("default-resource-info");
+  resourceInformation.classList.add(...resourceInfoClassesName);
+  main.appendChild(resourceTitle);
+  main.appendChild(resourceInformation);
 }
 
-generateCode("hey asdf and chase and snake are cool", ["blue", "large"]);
+generateCode(
+  "HTML (Hyper Text Markup Language)",
+  [],
+  "HTML is a a very popular markup language used for web development.",
+  []
+);
+
+generateCode(
+  "CSS (Cascading Style Sheets)",
+  ["blue", "medium"],
+  "CSS is a a very popular markup language used for web development."[
+    ("small", "lightblue")
+  ]
+);
+
+generateCode(
+  "Javascript",
+  [],
+  "Javascript is a a very popular markup language used for web development. It can be used for frontend and backend and is often used to make websites responsives instead of static.",
+  []
+);
